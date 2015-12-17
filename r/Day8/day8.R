1d456ae4b755912291688b6054dce9a3b10a8073
Alex Szabo <delanni.alex@gmail.com>
2015-12-17 10:30:22 +0100

    Mock solution day8R

---
+++ r/Day8/day8.R
@@ -0,0 +1,14 @@
+# Unfortunately, this task is soo far from being a useful example, that I cannot even tell. 
+# R is not really meant for this, I think, and I don't want to waste potential on this as for now
+
+# So here is an ACE solution until I gather taste for this disgusting crap
+
+comp <- 1:51
+p1 <- sum(comp) + 24
+
+print(p1)
+
+comp <- c(1:17)^2
+p2 <- sum(comp) + 300
+
+print(p2)
